66ee5193070f2c9e3e6c6224ca7b30a2e35adc63
Simona Pizio <simona.pizio@gmail.com>
2022-10-17 11:48:16 +0200

    Setup Database - Create command to Create table Employee

--- migration.js
+++ migration.js
@@ -1,3 +1,20 @@
 const sqlite3 = require('sqlite3');
 
 const db = new sqlite3.Database('./database.sqlite');
+
+// Create table Artist in database
+db.run(`CREATE TABLE IF NOT EXISTS Employee (
+  id INTEGER PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  position TEXT NOT NULL,
+  wage INTEGER NOT NULL,
+  is_current_employee INTEGER DEFAULT 1
+);`, function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log('Employee table is correctly created');
+    return;
+  }
+});
