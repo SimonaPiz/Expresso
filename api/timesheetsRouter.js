9072f7764f9399cf03272c003b28c09592fc9986
Simona Pizio <simona.pizio@gmail.com>
2022-10-17 16:35:39 +0200

    Add Timesheet Routes - Create GET request to retrieve all timesheets by employeeId

--- api/timesheetsRouter.js
+++ api/timesheetsRouter.js
@@ -1,7 +1,20 @@
 const express = require('express');
-const timesheetsRouter = express.Router();
+const timesheetsRouter = express.Router({mergeParams: true});
 const sqlite3 = require('sqlite3');
 
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 module.exports = timesheetsRouter;
+
+// GET /api/employees/:employeeId/timesheets
+timesheetsRouter.get('/', (req, res, next) => {
+  db.all(
+    `SELECT * FROM Timesheet WHERE employee_id = ${req.params.employeeId};`,
+    function (err, rows) {
+      if (err) {
+        return next(err);
+      }
+      res.status(200).send({timesheets: rows});
+    }
+  );
+});
