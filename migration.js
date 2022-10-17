1b09e63a19c535a5d5613f7460445d7a21a96c5e
Simona Pizio <simona.pizio@gmail.com>
2022-10-17 11:52:24 +0200

    Setup Database - Create command to Create table Timesheet

--- migration.js
+++ migration.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3');
 
 const db = new sqlite3.Database('./database.sqlite');
 
-// Create table Artist in database
+// Create table Employee in database
 db.run(`CREATE TABLE IF NOT EXISTS Employee (
   id INTEGER PRIMARY KEY NOT NULL,
   name TEXT NOT NULL,
@@ -15,6 +15,23 @@ db.run(`CREATE TABLE IF NOT EXISTS Employee (
     return;
   } else {
     console.log('Employee table is correctly created');
+    return;
+  }
+});
+
+// Create table Timesheet in database
+db.run(`CREATE TABLE IF NOT EXISTS Timesheet (
+  id INTEGER PRIMARY KEY NOT NULL,
+  hours INTEGER NOT NULL,
+  rate INTEGER NOT NULL,
+  date INTEGER NOT NULL,
+  employee_id INTEGER NOT NULL REFERENCES Employee(id)
+);`, function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log('Timesheet table is correctly created');
     return;
   }
 });
