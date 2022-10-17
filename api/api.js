ba6539717e68a2bb562c71f0a1a202371b20ce6c
Simona Pizio <simona.pizio@gmail.com>
2022-10-17 12:04:44 +0200

    Add Employee Routes - Create employeeRouter and connect it to apiRouter for quering database at path /api/employee

--- api/api.js
+++ api/api.js
@@ -2,3 +2,7 @@ const express = require('express');
 const apiRouter = express.Router();
 
 module.exports = apiRouter;
+
+//Import and connect employeeRouter for path 'api/employee'
+const employeeRouter = require('./employeeRouter');
+apiRouter.use('/employee', employeeRouter);
