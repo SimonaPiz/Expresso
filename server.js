fb8534fff48f8becd208aed60c987bb215277d15
Simona Pizio <simona.pizio@gmail.com>
2022-10-17 11:30:00 +0200

    Setup Server - Set PORT where server running

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ const express = require('express');
 const app = express();
 
 module.exports = app;
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, (req, res, next) => {
+  console.log('Server is running on port ' + PORT);
+})
