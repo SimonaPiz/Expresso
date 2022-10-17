18c4f4c5a6b01c037536fa93bef3ad059ca0b674
Simona Pizio <simona.pizio@gmail.com>
2022-10-17 11:35:40 +0200

    Setup Server - Connect body-parser, cors, errorHendler, morgan to app

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorHandler = require('errorhandler');
+const morgan = require('morgan');
 
 module.exports = app;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(errorHandler());
+app.use(morgan('dev'));
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 4000;
 
