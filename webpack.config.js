1487e97c34ac0f62e2cc83f0bfd524ddbc216a49
Simona Pizio <simona.pizio@gmail.com>
2022-10-17 11:21:23 +0200

    Set webpack configuration to update it

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,36 @@
 'use strict';
 
+var path = require('path');
 const webpack = require('webpack'); // eslint-disable-line no-unused-vars
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    path: __dirname,
+    path: path.resolve(__dirname, 'build'),
     filename: './public/js/bundle.js',
   },
   context: __dirname,
   devtool: 'source-map',
   module: {
-    loaders: [
+    rules: [
       {
-        test: /jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-2'],
-        }
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    ['@babel/preset-env', { targets: "defaults" }],
+                    ["@babel/preset-react"]
+                ]
+            }
+          },
       }
     ]
   },
   node: {
     fs: "empty"
-  }
+  },
+  devtool: 'source-map'
 };
