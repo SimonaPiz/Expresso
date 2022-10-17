42d0cb5efc7e5653ae9d1eedfb7a6ff9175ae2b6
Simona Pizio <simona.pizio@gmail.com>
2022-10-17 11:37:32 +0200

    Setup Server - Connect apiRouter to app express server

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(express.static('public'));
 
 const PORT = process.env.PORT || 4000;
 
+// Inport API Router and mount it at all routes starting at '/api'
+const apiRouter = require('./api/api');
+app.use('/api', apiRouter);
+
 app.listen(PORT, (req, res, next) => {
   console.log('Server is running on port ' + PORT);
 })
