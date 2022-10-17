3d9bf004b5d10c0a97eb51b0be8963b0955ee408
Simona Pizio <simona.pizio@gmail.com>
2022-10-17 13:57:40 +0200

    Add Employee Routes - Create POST request to create new employee

--- api/employeesRouter.js
+++ api/employeesRouter.js
@@ -18,3 +18,52 @@ employeesRouter.get('/', (req, res, next) => {
     }
   );
 });
+
+// Create function to validate new employee before adding to the database
+const validateData = (req, res, next) => {
+  const newData = req.body.employee;
+  //console.log(newData);
+
+  if (
+    typeof newData.name !== 'string' || !newData.name ||
+    typeof newData.position !== 'string' || !newData.position ||
+    newData.wage < 0 || !newData.wage
+  ) {
+    res.sendStatus(400);
+    return;
+  } else {
+    //console.log('Data are valid');
+    next();
+  }
+};
+
+// POST - Create new employee
+employeesRouter.post('/', validateData, (req, res, next) => {
+  //console.log(req.body.employee);
+  const newEmployee = req.body.employee;
+  newEmployee.isCurrentEmployee = req.body.employee.isCurrentEmployee === 0 ? 0 : 1;
+
+  db.run(
+    `INSERT INTO Employee (name, position, wage, is_current_employee)
+    VALUES (
+      '${newEmployee.name}',
+      '${newEmployee.position}',
+      '${newEmployee.wage}',
+      ${newEmployee.isCurrentEmployee}
+    );`,
+    function (err) {
+      if (err) {
+        return next(err);
+      }
+      db.get(
+        `SELECT * FROM Employee WHERE id = ${this.lastID};`,
+        (err, row) => {
+          if (err) {
+            return next(err);
+          }
+          res.status(201).send({employee: row});
+        }
+      );
+    }
+  );
+});
