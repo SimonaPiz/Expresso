064770bdf53817aa8b6760f84321a1fd671e144a
Simona Pizio <simona.pizio@gmail.com>
2022-10-18 08:39:08 +0200

    Add Timesheet Routes - Create PUT request to update a timesheet by its id

--- api/timesheetsRouter.js
+++ api/timesheetsRouter.js
@@ -6,6 +6,32 @@ const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite'
 
 module.exports = timesheetsRouter;
 
+// Add param ':timesheetId' to set it in all Router
+timesheetsRouter.param('timesheetId', (req, res, next, index) => {
+  const timesheetId = Number(index);
+  if (timesheetId && timesheetId >= 0) {  
+    db.get(
+      `SELECT * FROM Timesheet WHERE id = ?;`,
+      [timesheetId],
+      function (err, row) {
+        if (err) {
+          return next(err);
+        } else if (row) {
+          req.timesheet = row;
+          req.timesheetId = timesheetId;
+          next();
+        } else {
+          res.sendStatus(404);
+          return;
+        }
+      }
+    );
+  } else {
+    res.sendStatus(404);
+    return;
+  }
+});
+
 // GET /api/employees/:employeeId/timesheets
 timesheetsRouter.get('/', (req, res, next) => {
   db.all(
@@ -61,6 +87,32 @@ timesheetsRouter.post('/', validateData, (req, res, next) => {
             return next(err);
           }
           res.status(201).send({timesheet: row});
+        }
+      );
+    }
+  );
+});
+
+// PUT - Update timesheet by timesheetId
+timesheetsRouter.put('/:timesheetId', validateData, (req, res, next) => {
+  const updateTimesheet = req.body.timesheet;
+  db.run(
+    `UPDATE Timesheet SET 
+    hours = ${updateTimesheet.hours},
+    rate = ${updateTimesheet.rate},
+    date = ${updateTimesheet.date}
+    WHERE id = ${req.timesheetId};`,
+    function (err) {
+      if (err) {
+        return next(err);
+      }
+      db.get(
+        `SELECT * FROM Timesheet WHERE id = ${req.timesheetId};`,
+        (err, row) => {
+          if (err) {
+            return next(err);
+          }
+          res.status(200).send({timesheet: row});
         }
       );
     }
