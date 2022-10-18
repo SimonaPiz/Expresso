dc73cd46a2f0ae87c3c3aad0d577fb6cdccbefb9
Simona Pizio <simona.pizio@gmail.com>
2022-10-18 09:12:49 +0200

    Add MenuItems Routes - Create GET request to retrieve all items by menuId

--- api/menuItemsRouter.js
+++ api/menuItemsRouter.js
@@ -5,3 +5,16 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 module.exports = menuItemsRouter;
+
+// GET /api/menus/:menuId/menu-items
+menuItemsRouter.get('/', (req, res, next) => {
+  db.all(
+    `SELECT * FROM MenuItem WHERE menu_id = ${req.params.menuId};`,
+    function (err, rows) {
+      if (err) {
+        return next(err);
+      }
+      res.status(200).send({menuItems: rows});
+    }
+  );
+});
