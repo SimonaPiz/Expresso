bcfb28f7b6dde9c7847d07bb3afc6ec8a37f167e
Simona Pizio <simona.pizio@gmail.com>
2022-10-18 08:44:59 +0200

    Setup Server - Create menusRouter express to quering db for table menu

--- api/api.js
+++ api/api.js
@@ -3,6 +3,10 @@ const apiRouter = express.Router();
 
 module.exports = apiRouter;
 
-//Import and connect employeeRouter for path 'api/employee'
+//Import and connect employeesRouter for path 'api/employees'
 const employeesRouter = require('./employeesRouter');
 apiRouter.use('/employees', employeesRouter);
+
+//Import and connect menusRouter for path 'api/menus'
+const menusRouter = require('./menusRouter');
+apiRouter.use('/menus', menusRouter);
