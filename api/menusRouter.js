251f4c40d0c04a63be4177c2ab68917539738543
Simona Pizio <simona.pizio@gmail.com>
2022-10-18 08:51:53 +0200

    Add Menus Routes - Create POST request to create new menu

--- api/menusRouter.js
+++ api/menusRouter.js
@@ -18,3 +18,44 @@ menusRouter.get('/', (req, res, next) => {
     }
   );
 });
+
+// Create function to validate new menu before adding to the database
+const validateData = (req, res, next) => {
+  const newData = req.body.menu;
+  //console.log(newData);
+
+  if (
+    typeof newData.title !== 'string' || !newData.title
+  ) {
+    res.sendStatus(400);
+    return;
+  } else {
+    //console.log('Data are valid');
+    next();
+  }
+};
+
+// POST - Create new menu
+menusRouter.post('/', validateData, (req, res, next) => {
+  //console.log(req.body.menu);
+  const newMenu = req.body.menu;
+
+  db.run(
+    `INSERT INTO Menu (title)
+    VALUES ('${newMenu.title}');`,
+    function (err) {
+      if (err) {
+        return next(err);
+      }
+      db.get(
+        `SELECT * FROM Menu WHERE id = ${this.lastID};`,
+        (err, row) => {
+          if (err) {
+            return next(err);
+          }
+          res.status(201).send({menu: row});
+        }
+      );
+    }
+  );
+});
