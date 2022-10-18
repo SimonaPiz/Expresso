868e7e67cfc5a520a154ae0b1d922bf29bdcb052
Simona Pizio <simona.pizio@gmail.com>
2022-10-18 09:20:14 +0200

    Add MenuItems Routes - Create POST request to create new manuItem by menu id

--- api/menuItemsRouter.js
+++ api/menuItemsRouter.js
@@ -18,3 +18,53 @@ menuItemsRouter.get('/', (req, res, next) => {
     }
   );
 });
+
+// Create function to validate new menuItem before adding to the database
+const validateData = (req, res, next) => {
+  const newData = req.body.menuItem;
+  //console.log(newData);
+
+  if (
+    typeof newData.name !== 'string' || !newData.name ||
+    typeof newData.description !== 'string' ||
+    newData.inventory < 0 || !newData.inventory ||
+    newData.price < 0 || !newData.price
+  ) {
+    res.sendStatus(400);
+    return;
+  } else {
+    //console.log('Data are valid');
+    next();
+  }
+};
+
+// POST - Create new menuItem
+menuItemsRouter.post('/', validateData, (req, res, next) => {
+  //console.log(req.body.menuItem);
+  const newMenuItem = req.body.menuItem;
+
+  db.run(
+    `INSERT INTO MenuItem (name, description, inventory, price, menu_id)
+    VALUES (
+      '${newMenuItem.name}',
+      '${newMenuItem.description}',
+      ${newMenuItem.inventory},
+      ${newMenuItem.price},
+      ${req.params.menuId}
+    );`,
+    function (err) {
+      if (err) {
+        return next(err);
+      }
+      db.get(
+        `SELECT * FROM MenuItem WHERE id = ${this.lastID};`,
+        (err, row) => {
+          if (err) {
+            return next(err);
+          }
+          res.status(201).send({menuItem: row});
+        }
+      );
+    }
+  );
+});
